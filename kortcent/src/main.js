d309e213994d254e703766415ae81d8d53b37800
Kortnee Rong <kortneer@nycstudents.net>
2026-04-29 12:29:44 -0400

    6ytr

--- kortcent/src/main.js
+++ kortcent/src/main.js
@@ -4,9 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
