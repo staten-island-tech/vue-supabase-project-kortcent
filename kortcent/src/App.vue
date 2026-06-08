930082af363f65f028ec3cc9ee0a1f773b208a1e
Kortnee Rong <kortneer@nycstudents.net>
2026-06-07 23:21:19 -0400

    s

--- kortcent/src/App.vue
+++ kortcent/src/App.vue
@@ -3,20 +3,43 @@ import { ref, onMounted } from 'vue'
 import { createClient } from '@supabase/supabase-js'
 
 const todos = ref([])
+const transactions = ref([])
+const error = ref(null)
+const supabase = createClient(URL, KEY)
 
 async function getTodos() {
   const { data } = await supabase.from('todos').select()
   todos.value = data
 }
 
-onMounted(() => {
+onMounted(async () => {
   getTodos()
+  let { data: transactionData, error: err } = await supabase
+    .from('transactions')
+    .select('*')
+  if (err) {
+    error.value = err.message
+  } else {
+    transactions.value = transactionData
+  }
 })
 </script>
 
 <template>
-  <ul>
+  <ul v-if="todos.length">
     <p>hi</p>
-    <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.name }}
+    </li>
+  </ul>
+
+  <ul v-else>
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+    >
+      ID: {{ transaction.id }}
+      User: {{ transaction.user_id }}
+    </li>
   </ul>
 </template>
--- kortcent/src/router/index.js
+++ kortcent/src/router/index.js
@@ -1,4 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
