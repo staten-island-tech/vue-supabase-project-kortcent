428ade306bfcc3fca82ab7bf43d1a853b55193f4
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-06-15 01:19:02 -0400

    Update main.js

--- kortcent/src/main.js
+++ kortcent/src/main.js
@@ -5,8 +5,6 @@ import App from './App.vue'
 import router from './router'
 import { useAuthStore } from './stores/authStore'
 
-import './assets/main.css'
-
 const app = createApp(App)
 
 app.use(createPinia())
