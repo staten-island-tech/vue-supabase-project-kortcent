e7873a57cb5ff7fce247ac4a5fd268b84ad629c0
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-06-15 04:15:55 -0400

    all good

--- kortcent/src/router/index.js
+++ kortcent/src/router/index.js
@@ -64,11 +64,16 @@ const router = createRouter({
   routes,
 })
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const auth = useAuthStore()
 
+  // Make sure session is restored before checking
+  if (!auth.user) {
+    await auth.restoreSession()
+  }
+
   if (to.meta.requiresAuth && !auth.isLoggedIn) {
-    next({ name: 'login' })
+    next({ name: 'home' })
   } else if ((to.name === 'login' || to.name === 'register') && auth.isLoggedIn) {
     next({ name: 'profile' })
   } else {
